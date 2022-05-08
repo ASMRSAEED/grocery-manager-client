b45905c83847eba806b3a9013ff7649431dc89ba
ASMRSAEED <asmrsaeed@gmail.com>
2022-05-08 13:41:06 +0600

    Soaial Icon Added

--- src/components/Shared Pages/Footer/Footer.js
+++ src/components/Shared Pages/Footer/Footer.js
@@ -7,18 +7,29 @@ const Footer = () => {
   return (
     <div>
       <section className="footer">
-        <div className="shares">
-          <Link to="#" className="btn"><BsFacebook></BsFacebook></Link>
-          <Link to="#" className="btn"><BsTwitter></BsTwitter></Link>
-          <Link to="#" className="btn"><BsInstagram></BsInstagram></Link>
-          <Link to="#" className="btn"><BsPinterest></BsPinterest></Link>
-          <Link to="#" className="btn"><BsLinkedin></BsLinkedin></Link>
-          <Link to="#" className="btn"><BsMedium></BsMedium></Link>
-          <Link to="#" className="btn"><BsYoutube></BsYoutube></Link>
-          <Link to="#" className="btn"><BsWhatsapp></BsWhatsapp></Link>
+        <div className='footer-details'>
+          <div className="social-icon">
+            <h2>Stay connected</h2>
+            <Link to="#" ><BsFacebook></BsFacebook></Link>
+            <Link to="#" ><BsTwitter></BsTwitter></Link>
+            <Link to="#" ><BsInstagram></BsInstagram></Link>
+            <Link to="#" ><BsWhatsapp></BsWhatsapp></Link>
+          </div>
+          <div className='footer-middle'>
+            <h2>Shopping with us</h2>
+            <Link to="#" >Making payments</Link>
+            <Link to="#" >Delivery options</Link>
+            <Link to="#" >Buyer Protection</Link>
+            <Link to="#" ></Link>
+          </div>
+          <div className='footer-end'>
+            <h2>Customer service</h2>
+            <Link to="#" >Transaction Services Agreement</Link>
+            <Link to="#" >Take our feedback survey</Link>
+          </div>
         </div>
 
-        <h1 className="credit">
+        <h1 className="copyright">
           Copyright@ Grocery Manager
           | All Rights Reserved! 2022
         </h1>
